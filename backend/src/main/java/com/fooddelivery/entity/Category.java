dfda3d67f3d69087f6ef2aca077e4960d73daf5f
PriyamJaiswal <priyamj608@gmail.com>
2026-04-28 16:00:00 +0530

    feat: add Category enum for food classification

---
+++ backend/src/main/java/com/fooddelivery/entity/Category.java
@@ -0,0 +1,19 @@
+package com.fooddelivery.entity;
+
+/**
+ * Enum representing food item categories available in the food delivery system.
+ */
+public enum Category {
+    PIZZA,
+    BURGER,
+    DRINK,
+    DESSERT,
+    INDIAN,
+    CHINESE,
+    SOUTH_INDIAN,
+    CAKE,
+    ROLLS,
+    NOODLES,
+    COFFEE,
+    HEALTHY
+}
